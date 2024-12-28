8faf5d295483fe9e1bf6fa10347e64771f35b595
someshwarsholkar <someshwarsholkar22@gmail.com>
2024-12-28 10:34:44 +0530

    token error fix

--- middleware/Protected.ts
+++ middleware/Protected.ts
@@ -7,18 +7,8 @@ interface CustomRequest extends Request {
 
 export const userProtected = async (req: CustomRequest, res: Response, next: NextFunction): Promise<any> => {
 
-    console.log(req.cookies, "DDDDDDDDDDDDDDDDD");
-
-    // if (!req.cookies) {
-    //     return res.status(400).json({ message: "cookies not founddd" })
-    // }
-    const { user } = req.cookies;
-    console.log(user);
-
-
-    if (!user) {
-        return res.status(401).json({ message: 'Authorization token is missing. Please login first.  message from shareable protected' });
-    }
+    const user: any = req.headers.authorization
+    console.log(user, "USER TOKEN")
 
     jwt.verify(user, process.env.JWT_KEY as string, async (error: VerifyErrors | null, decoded: any) => {
         if (error) {
@@ -26,15 +16,9 @@ export const userProtected = async (req: CustomRequest, res: Response, next: Nex
             if (error.name === 'TokenExpiredError') {
                 errorMessage = 'Token has expired. Please login again.';
             }
-
             return res.status(401).json({ message: errorMessage });
         }
-
-        // Attach decoded user information to the request object
         req.user = decoded.userId;
-        console.log(req.user, "from proctedddd");
-
-        // Proceed to the next middleware or route handler
         next();
     });
 };
